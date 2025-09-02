76f8fe3962c900096751bb1707ed84b918b440f8
Luis Pinillos <pinillosluisfer@gmail.com>
2025-09-01 23:55:11 -0500

    first commit

--- src/main/java/co/analisys/biblioteca/controller/CatalogoController.java
+++ src/main/java/co/analisys/biblioteca/controller/CatalogoController.java
@@ -4,6 +4,7 @@ import co.analisys.biblioteca.model.Libro;
 import co.analisys.biblioteca.model.LibroId;
 import co.analisys.biblioteca.service.CatalogoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,22 +20,26 @@ public class CatalogoController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_USER')")
     public Libro obtenerLibro(@PathVariable String id) {
         return catalogoService.obtenerLibro(new LibroId(id));
     }
 
     @GetMapping("/{id}/disponible")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_USER')")
     public boolean isLibroDisponible(@PathVariable String id) {
         Libro libro = catalogoService.obtenerLibro(new LibroId(id));
         return libro != null && libro.isDisponible();
     }
 
     @PutMapping("/{id}/disponibilidad")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_USER')")
     public void actualizarDisponibilidad(@PathVariable String id, @RequestBody boolean disponible) {
         catalogoService.actualizarDisponibilidad(new LibroId(id), disponible);
     }
 
     @GetMapping("/buscar")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_USER')")
     public List<Libro> buscarLibros(@RequestParam String criterio) {
         return catalogoService.buscarLibros(criterio);
     }
